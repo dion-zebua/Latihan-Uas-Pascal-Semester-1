84e02417cbc69bc09086e2edebf45580fdf58e42
Kurniawann12 <kurniawanahkam@gmail.com>
2024-01-08 19:13:35 +0700

    Lepasin Comment

--- #1 deretaKelipatan5/index.pas
+++ #1 deretaKelipatan5/index.pas
@@ -1,24 +1,24 @@
-// program kelipatan;
-// uses crt;
-// var 
-//     n, i, k, d, t: integer;
-// begin
-//     clrscr;
-//     write('Input sebuah angka : ');
-//     readln(n);
-//     i := 1; k := 5;
-//     writeln('====================');
-//     write('Deret Bilangan : ');
-//     while (i <= n) do
-//     begin
-//         d := k * i;
-//         write(d , ' ');
-//         i := i + 1;
-//         t := t + d;
-//     end;
-//     writeln();
-//     writeln('Total : ' , t);
-// end.
+program kelipatan;
+uses crt;
+var 
+    n, i, k, d, t: integer;
+begin
+    clrscr;
+    write('Input sebuah angka : ');
+    readln(n);
+    i := 1; k := 5;
+    writeln('====================');
+    write('Deret Bilangan : ');
+    while (i <= n) do
+    begin
+        d := k * i;
+        write(d , ' ');
+        i := i + 1;
+        t := t + d;
+    end;
+    writeln();
+    writeln('Total : ' , t);
+end.
 
 program kelipatan;
 uses crt;
